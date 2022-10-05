79bb1f27a7db0a058bbd2acda47cbda2e8c650de
Baaasty <46427883+Baaasty@users.noreply.github.com>
2022-10-05 12:43:34 +0200

    try functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hello world</h1>
-    </div>
-  );
+    const name = "Bastian";
+    const isMale = true;
+
+    function getGreeting(user) {
+        if (user) {
+            return <h1>Hello, {user}!</h1>;
+        }
+        return <h1>Hello, Stranger.</h1>
+    }
+
+    return (
+        <div className="app">
+            {getGreeting(name)}
+            <h2>You are a {isMale ? "Male" : "Female"}</h2>
+        </div>
+    );
 }
 
 export default App;
